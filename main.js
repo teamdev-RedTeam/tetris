3226189d5c3caa9261a5c144c7605b760eb02c9f
KoshinHagimoto <koshin.hagimoto@gmail.com>
2023-04-25 14:36:48 +0900

    socre level

--- main.js
+++ main.js
@@ -48,8 +48,6 @@ let tetro_x = START_X;
 let tetro_y = START_Y;
 
 let id;
-// レベルが上がるごとに速くする
-let dropSpeed = 600;
 let gameOver = false;
 
 const TETRO_COLORS = [
@@ -65,10 +63,12 @@ const TETRO_COLORS = [
     [0, 0, 0],          //9黒
 ];
 
-// 消したライン数
+// スコア・レベル
+const DROP_SPEED_INTERVAL = 100; //レベルアップするごとにアップする速度[msec]
+const MAX_LEVEL = 10;
 let lines = 0;
-// スコア
 let score = 0;
+let level = 1;
 
 // 7種類のテトロミノ
 const TETRO_PATTERN = [
@@ -263,8 +263,15 @@ function checkLine(){
     // スコア用の処理
     if(linec) {
         lines += linec;
-        score += 100 * (2**(linec-1));
-        // スコアと消したライン数を表示
+        score += level * 10 * linec ** 2;
+        if(level <= MAX_LEVEL) {
+            for(i=level; i<=MAX_LEVEL; i++) {
+                if(score >= (level+1)**3 * 4) {
+                    level += 1;
+                }
+            }
+        }
+        document.getElementById("level").innerHTML = level;
         document.getElementById("lines").innerHTML = lines;
         document.getElementById("score").innerHTML = score;
     }
@@ -401,7 +408,7 @@ function initGame() {
 
     id = setInterval(() => {
         dropTetro();
-    }, dropSpeed);
+    }, 1000-(level-1)*DROP_SPEED_INTERVAL);
 }
 
 // リセット
@@ -454,7 +461,7 @@ document.getElementById("pauseBtn").addEventListener("click", () => {
         btn.innerHTML = paused;
         id = setInterval(() => {
             dropTetro();
-        }, dropSpeed);
+        }, 1000-(level-1)*DROP_SPEED_INTERVAL);
     }
 });
 
